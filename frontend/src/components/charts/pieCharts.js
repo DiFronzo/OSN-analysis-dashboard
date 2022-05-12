ffa763de317e6bfb171abf7b716b55703517553a
Andreas <andrklie@stud.ntnu.no>
2022-05-12 13:27:56 +0200

    +pie chart support

---
+++ frontend/src/components/charts/pieCharts.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+class PieChart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chartData: [],
+      chartOptions: {}
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      chartData: this.props.chartData,
+      chartOptions: this.props.chartOptions
+    });
+  }
+
+  render() {
+    return (
+      <ReactApexChart
+        options={this.state.chartOptions}
+        series={this.state.chartData}
+        type="pie"
+
+      />
+    );
+  }
+}
+
+export default PieChart;
